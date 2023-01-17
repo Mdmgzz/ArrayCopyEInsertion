1072b4bf8778f1dcd36f6f9f18aac21c52247aab
mdominguezg <mdominguezg@106-06.info2>
2023-01-17 12:33:08 +0100

    ejercicio4 completado

--- ArraysCopiaEInsercion/src/ejercicios/Ejercicio04.java
+++ ArraysCopiaEInsercion/src/ejercicios/Ejercicio04.java
@@ -1,5 +1,6 @@
 package ejercicios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio04 {
@@ -8,7 +9,7 @@ public class Ejercicio04 {
 		// Bloque de variables
 		int tablaenteros[]=new int[10];
 		int ordenados[]= new int [0];
-		int rastreador;
+		int rastreador=0;
 
 		
 		Scanner sc= new Scanner(System.in);
@@ -16,8 +17,21 @@ public class Ejercicio04 {
 			System.out.println("Introduce un numero: ");
 			tablaenteros[i]=sc.nextInt();
 		}
-	
+		Arrays.sort(tablaenteros);
 		
+		for (int i=0;i<=tablaenteros.length-1;i++) {
+			if (i==0 || tablaenteros[i]!=tablaenteros[i-1]) {
+				ordenados=Arrays.copyOf(ordenados, ordenados.length+1);
+				ordenados[rastreador++]=tablaenteros[i];
+			}
+		
+		}
+		System.out.println(Arrays.toString(tablaenteros));
+			
+		System.out.println(Arrays.toString(ordenados));
+	
+		//cerramos scanner
+		sc.close();
 		
 	}
 
